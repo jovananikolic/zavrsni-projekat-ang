c2b32fe8bedaf9f688aa5c97db6406f8c76b2b51
jovananik <jovana.nikolic@cubes.rs>
2018-02-05 15:57:20 +0100

    Importovanje index stranice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit  } from '@angular/core';
+import * as $ from 'jquery';
+
+// declare var jquery:any;
+// declare var $ :any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public ngOnInit() {
+    $('.toggle-menu').click(function(){
+        $('.menu-overlay').fadeIn();
+        $('.menu').addClass('active');
+        $('body').addClass('inactive');
+    });
+
+    $('.menu-overlay').click(function(){
+        $(this).fadeOut();
+        $('.menu').removeClass('active');
+        $('body').removeClass('inactive');
+    });
+  }
+
+  // ngAfterViewInit() {
+  //   $('.toggle-menu').click(function(){
+  //       $('.menu-overlay').fadeIn();
+  //       $('.menu').addClass('active');
+  //       $('body').addClass('inactive');
+  //   });
+  //
+  //   $('.menu-overlay').click(function(){
+  //       $(this).fadeOut();
+  //       $('.menu').removeClass('active');
+  //       $('body').removeClass('inactive');
+  //   });
+  // }
 }
